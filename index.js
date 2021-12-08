c2b3cf1d0b3ecc05a6e66b2dea1f1d0dda52725f
Gary Flugel <flugelg@gmail.com>
2021-12-08 14:51:12 -0500

    commit tree 7df2e97558e444cb54dfd10aad6e6aa5646b2152

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(array, name){
+    const user = array.filter(person => person.toUpperCase() === name.toUpperCase());
+    return user;
+}
+
+function fuzzyMatch(array, fuzz){
+    const user = array.filter(person => person.startsWith(fuzz));
+    return user;
+}
+
+function matchName(obj, name){
+    const user = obj.filter(person => person.name === name);
+    return user;
+}
